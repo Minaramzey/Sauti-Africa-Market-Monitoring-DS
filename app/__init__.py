dde0d673290e22d5ef2926f18c2f56e01a22140b
Coding Duck <57272996+CodingDuckmx@users.noreply.github.com>
2020-06-15 19:29:08 -0500

    CORS added

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 import urllib.parse as up
 
 from flask import Flask
-
+from flask_cors import CORS
 
 def create_app():
 
     app = Flask(__name__, instance_relative_config=False) # The configuration is comming from other file.
     app.config.from_object('config.Config')
+    CORS(app)
 
     with app.app_context():
 
